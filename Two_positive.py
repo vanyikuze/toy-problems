acd0c86c15f4d97902853e02d85517aff57d2095
Vany <vany.ikuze@student.moringaschool.com>
2023-08-22 17:07:14 +0300

    Added Two Positive

---
+++ Two_positive.py
@@ -0,0 +1,13 @@
+def two_positive(a, b, c) :
+    if a > 0 and b > 0 and c > 0 :
+        return "True"
+    elif a < 0 and b > 0 and c > 0 :
+        return "True"
+    elif a > 0 and b < 0 and c > 0 :
+        return "True"
+    elif a > 0 and b > 0 and c < 0 :
+        return "True"
+    else :
+        return "False"
+    
+    
